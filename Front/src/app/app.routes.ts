ff398bdb0093de8e4efd86d06b1a1e5c22374c04
ThaysonScript <thaysonguedes01@gmail.com>
2026-08-05 17:33:20 -0300

    feat(auth-front): adicionado funcionalidade de logout base

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LogoutComponent } from './autenticacao/logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'logout', component: LogoutComponent},
+];
