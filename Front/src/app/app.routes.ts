f56cda64356919b82a53d09ce09855dbb252049f
ThaysonScript <thaysonguedes01@gmail.com>
2026-08-12 22:54:08 -0300

    feat(navigation): registrar rota atividades/cadastro e adicionar link no dashboard

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './autenticacao/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressoComponent } from './atividades/progresso/progresso.component';
 import { authGuard } from './autenticacao/auth.guard';
+import { CadastroAtividadeComponent } from './atividades/cadastro/cadastro-atividade.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { path: 'registro', component: RegistroComponent },
     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
     { path: 'progresso', component: ProgressoComponent, canActivate: [authGuard] },
+    { path: 'atividades/cadastro', component: CadastroAtividadeComponent, canActivate: [authGuard] },
     { path: 'logout', component: LogoutComponent, canActivate: [authGuard] },
     { path: '**', redirectTo: 'login' }
 ];
