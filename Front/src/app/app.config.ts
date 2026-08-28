edd310ceae07bb9ac23ddd6f9342c444c6201ee4
ThaysonScript <thaysonguedes01@gmail.com>
2026-08-28 20:49:49 -0300

    feature(front): mocks para a tela de notificações

--- Front/src/app/app.config.ts
+++ Front/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { AuthInterceptor } from './autenticacao/auth.interceptor';
+import { mockApiInterceptor } from './core/interceptors/mock-api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([AuthInterceptor])),
+    // mockApiInterceptor intercepta antes ou depois do AuthInterceptor conforme a ordem do array
+    provideHttpClient(withInterceptors([AuthInterceptor, mockApiInterceptor])),
   ],
 };
